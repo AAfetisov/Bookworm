15a29aac81e40f5629ccd6b7b076a7a2a8290b5c
Akhmed <nob_32@mail.ru>
2023-02-21 14:35:54 +0300

    db_end

---
+++ src/db/migrations/20230221105946-create-book.js
@@ -0,0 +1,46 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'Users',
+          },
+          key: 'id',
+        },
+      },
+      img: {
+        type: Sequelize.TEXT,
+      },
+      title: {
+        type: Sequelize.TEXT,
+      },
+      author: {
+        type: Sequelize.TEXT,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  },
+};

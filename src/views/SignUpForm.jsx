e1f618c26ef85671305afaa9cdf70de29e53248d
Александр Фетисов <roumor@gmail.com>
2023-02-22 23:43:04 +0700

    signing navbar fix

--- src/views/SignUpForm.jsx
+++ src/views/SignUpForm.jsx
@@ -1,32 +1,32 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-function SignUpForm({ user = {} }) {
+function SignUpForm({ user }) {
   return (
     <Layout user={user}>
       <>
-        <div className='title-login'>
+        <div className="title-login">
           <h2>Регистрация</h2>
         </div>
 
-        <div className='cont'>
-          <form id='authSignUpForm' action='/auth/signup' method='post'>
-            <label htmlFor='name'>Name:</label>
-            <input className="login-input" type='text' name='name' id='name' />
-            <label htmlFor='email'>E-mail:</label>
-            <input className="login-input" type='text' name='email' id='email' />
-            <label htmlFor='password'>Password:</label>
-            <input className="login-input" type='password' name='password' id='password' />
-            <button id='authSubmitBtn' className='button' type='submit'>
+        <div className="cont">
+          <form id="authSignUpForm" action="/auth/signup" method="post">
+            <label htmlFor="name">Name:</label>
+            <input className="login-input" type="text" name="name" id="name" />
+            <label htmlFor="email">E-mail:</label>
+            <input className="login-input" type="text" name="email" id="email" />
+            <label htmlFor="password">Password:</label>
+            <input className="login-input" type="password" name="password" id="password" />
+            <button id="authSubmitBtn" className="button" type="submit">
               Register
             </button>
-            <div id='errText' className='authErrTxt'>
+            <div id="errText" className="authErrTxt">
               Wrong E-mail or Password
             </div>
           </form>
         </div>
       </>
-      <script src='/js/signup.js' />
+      <script src="/js/signup.js" />
     </Layout>
   );
 }

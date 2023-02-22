80a04a6e443986e61b8dfe5409276e99af40581b
Olga <olvik1605@gmail.com>
2023-02-22 21:16:03 +0300

    fixed my profile

--- src/views/myProfile/Myprofile.jsx
+++ src/views/myProfile/Myprofile.jsx
@@ -6,41 +6,64 @@ function MyProfile({ user, posts }) {
   const proverka = posts.length > 0;
   return (
     <Layout user={user} title={titleName}>
-
-      <script defer src="/js/Book.js" />
-      <div className="MyProfile">
-        <h2>My books:</h2>
-
+      <script defer src='/js/Book.js' />
+      <div className='MyProfile'>
+        <div className='textProfile'>
+          <h2>My books:</h2>
+        </div>
 
         {proverka > 0 ? (
           posts.map((e) => (
-            <div key={e.id} className="PostContainer" id={e.id}>
-              <div className="leftBox">
-                <img className="imgPost" src={e.img} alt={e.title} />
+            <div key={e.id} className='PostContainer' id={e.id}>
+              <div className='leftBox'>
+                <img className='imgPost' src={e.img} alt={e.title} />
               </div>
-              <div className="rigthBox">
-                <h5>{e.title}</h5>
-                <p>{e.author}</p>
-                <div>
+              <div className='rigthBox'>
+                <div className='bookTitle'>
+                  <h2>{e.title}</h2>
+                </div>
+
+                <div  className='author'>
+                  <p>Author: {e.author}</p>
+                </div>
+
+                <div class='star-rating'>
+                  <input type='radio' name='stars' id='star-a' value='5' />
+                  <label for='star-a'></label>
+
+                  <input type='radio' name='stars' id='star-b' value='4' />
+                  <label for='star-b'></label>
+
+                  <input type='radio' name='stars' id='star-c' value='3' />
+                  <label for='star-c'></label>
+
+                  <input type='radio' name='stars' id='star-d' value='2' />
+                  <label for='star-d'></label>
+
+                  <input type='radio' name='stars' id='star-e' value='1' />
+                  <label for='star-e'></label>
+                </div>
+
+                <div className='text-info'>
                   <p>{e.description}</p>
                 </div>
 
                 <div>
                   <button
-                    name="buttonEditPostForm"
-                    id="buttonEditPostForm"
-                    type="submit"
-                    data-type="addbook-button"
-                    className="buttonEditPostForm"
+                    name='buttonEditPostForm'
+                    id='buttonEditPostForm'
+                    type='submit'
+                    data-type='addbook-button'
+                    className='buttonEditPostForm'
                   >
                     Change this post!
                   </button>
-                  <div className="message" />
+                  <div className='message' />
                   <button
-                    id="buttonDeletePost"
-                    type="submit"
-                    data-type="addbook-button"
-                    className="buttonDeletePost"
+                    id='buttonDeletePost'
+                    type='submit'
+                    data-type='addbook-button'
+                    className='buttonDeletePost'
                   >
                     delete this post!
                   </button>
@@ -49,12 +72,10 @@ function MyProfile({ user, posts }) {
             </div>
           ))
         ) : (
-          <h5>
-            К сожалению вы ничего не опубликовали на данный момент
-          </h5>
+          <h5>К сожалению вы ничего не опубликовали на данный момент</h5>
         )}
       </div>
-      <script defer src="/js/Book.js" />
+      <script defer src='/js/Book.js' />
     </Layout>
   );
 }

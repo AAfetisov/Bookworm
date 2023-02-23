43885f7b4ec520617f7dfe49772ac71433fb2a33
dtumen <dt1109@mail.ru>
2023-02-23 15:46:10 +0100

    sth done

--- src/views/FavoritesView.jsx
+++ src/views/FavoritesView.jsx
@@ -2,8 +2,16 @@ const React = require('react');
 const Layout = require('./Layout');
 
 module.exports = function BookView({ user, favs }) {
+  console.log(favs, 'favsINView---->');
   <Layout user={user}>
-
-    213
+    {favs ? (
+      favs.map((e) => (
+        <div key={e.id}>
+          <img className="img-entries" src={e.img} alt={e.title} />
+          <p>{e.title}</p>
+          <p>{e.author}</p>
+          <p>{e.description}</p>
+        </div>
+      ))) : <p>К сожалению вы ничего не добавили</p>}
   </Layout>;
 };
